afe10fc986e9af6b71f18c955e75fc5d7dd782bd
Matt Barker <matt@barker.space>
2016-09-28 21:52:19 +1000

    Update CatFacts.java

--- CatFacts.java
+++ CatFacts.java
@@ -1,73 +1,84 @@
-
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package catfacts;
-import static java.awt.SystemColor.text;
+
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
-import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Properties;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
- * @author mbarker
+ * @author Matt
  */
 public class CatFacts {
 
- 
-    public static void main(String[] args) throws InterruptedException {
-     boolean senderSwitch = true;   
-     int emailFrequency = 2 * 1000; // PAUSE IN SECS
-     String emailRecipient = "";
-     String from = "mr.whiskers@catfacts.com";
-     String fact = "";
-     String subject ="";
-     ArrayList<String>emailSubject = new ArrayList<>();
-     emailSubject.add("IN COMMING CAT FACT");
-     emailSubject.add("HEY CAT LOVER");        
-     emailSubject.add("BECAUSE I KNOW YOU LOVE CATS:");
-     emailSubject.add("MEOWTASTIC!!!!!");
-     emailSubject.add("WELL HELLO THERE");
-     ArrayList<String> catGreeting = new ArrayList<>();
-     catGreeting.add("IN COMMING CAT FACT");
-     catGreeting.add("HEY CAT LOVER");        
-     catGreeting.add("BECAUSE I KNOW YOU LOVE CATS:");
-     catGreeting.add("MEOWTASTIC!!!!!");
-     catGreeting.add("WELL HELLO THERE");
-     ArrayList<String> catFact = new ArrayList<>();
-     catFact.add("Unlike dogs, cats do not have a sweet tooth. Scientists believe this is due to a mutation in a key taste receptor.");
-     catFact.add("There are more than 500 million domestic cats in the world, with approximately 40 recognized breeds.");        
-     catFact.add("Approximately 24 cat skins can make a coat");
-     catFact.add("During the time of the Spanish Inquisition, Pope Innocent VIII condemned cats as evil and thousands of cats were burned. Unfortunately, the widespread killing of cats led to an explosion of the rat population, which exacerbated the effects of the Black Death.");
-     catFact.add("The first cat in space was a French cat named Felicette (a.k.a. “Astrocat”) In 1963, France blasted the cat into outer space. Electrodes implanted in her brains sent neurological signals back to Earth. She survived the trip.");
-     ArrayList<String> catGoodbye = new ArrayList<>();
-     catGoodbye.add("Bye");
-     catGoodbye.add("Cya");
-     catGoodbye.add("Goodbye");
-     catGoodbye.add("Meow You Later");
-     catGoodbye.add("Goodbye");
-       while (senderSwitch){
-                int randomSubject = (int )(Math.random() * 5); //Keep the numbers to the size of the Array.
-                int randomGreeting = (int )(Math.random() * 5); 
-                int randomFact = (int )(Math.random() * 5); 
-                int randomGoodbye = (int )(Math.random() * 5); 
-
-              /*  System.out.println
-                   ("Greeting Number: " +randomGreeting + " " +
-                    "Fact Number " + randomFact + " " + 
-                    "Goodbye Number " + randomGoodbye);
-
-              */
-
-              subject = emailSubject.get(randomSubject);
-              fact = catGreeting.get(randomGreeting) + " " + catFact.get(randomFact) + " " + catGoodbye.get(randomGoodbye);
-
-
-              System.out.println ("FROM: " + from);
-              System.out.println ("EMAIL SUBJECT LINE: " + subject);
-              System.out.println ("\n");
-              System.out.println ("EMAIL CONTENTS: " + fact);
-              
-              Thread.sleep(emailFrequency);
    
-       }
+    public static void main(String[] args ) {
+    boolean factLoop = true;
+    int factsSent = 0;
+    int factSleep = 2 * 1000;
+    CatFacts cat = new CatFacts();      
+    while(factLoop){
+    String fact = cat.getFact();
+    printFact(fact);    
+    factsSent++;
+    System.out.println("Facts sent: " + factsSent);     
+        try {
+            Thread.sleep(factSleep);
+        } catch (InterruptedException ex) {
+             System.err.println("TIMER ERROR");
+        }
     }
     
+    }
+    
+  public String getFact() { 
+    String factToken = "";
+    int arraySize = 0;
+    int randomFactNumber = 0;
+    String fact = "";
+    Scanner inFile1 = null;
+    try {
+        inFile1 = new Scanner(new File("FactSheet.txt")).useDelimiter(",\\s*");
+    } catch (FileNotFoundException ex) {
+           System.err.println("CAT FACTSHEET NOT FOUND");
+    }
+    ArrayList<String> catFactsArray = new ArrayList<String>();
+    while (inFile1.hasNextLine()) {
+      factToken = inFile1.nextLine();
+      catFactsArray.add(factToken);
+      arraySize++;
+    }
+    inFile1.close();
+   System.out.println("Facts compiled successfully");
+   System.out.println("Number of facts compiled:" + arraySize);
+
+   randomFactNumber = (int )(Math.random() * arraySize);
+   fact = catFactsArray.get(randomFactNumber); 
+    return fact;
 }
+    
+  public static void printFact(String fact){  
+  System.out.println(fact);
+  }
+  
+  public static void sendEmail(String fact){
+
+      
+  }
+  
+  
+  
+}
+    
+
+
+    
